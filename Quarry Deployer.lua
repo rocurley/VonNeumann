08c49e7480579286351e228cd9cd0125e9905224
Roger Curley <rocurley@gmail.com>
2014-01-22 22:11:27 -0500

    Derp 2

--- Quarry Deployer.lua
+++ Quarry Deployer.lua
@@ -358,7 +358,7 @@ function backI(n)
     end
 end
 function loadUp()
-    sort=perpheral.wrap("back")
+    sort=perpheripheral.wrap("back")
     while turtle.getItemCount(1)<1 do
         sort.extract(3,getUUID(2671,0),2,1)
     end
