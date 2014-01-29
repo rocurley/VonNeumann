4bec87f0bc069f2d9307aa5731e2dc84b83a5079
Roger Curley <rocurley@gmail.com>
2014-01-29 14:56:29 -0500

    Added protection from disconnects

--- Base Manager.lua
+++ Base Manager.lua
@@ -17,27 +17,31 @@ ae=peripheral.wrap("bottom")
 cycle=0
 while true do
     inventory=ae.listItems()
-    jobsCount=0
-    if inventory[getUUID(4,0)]~=nil and inventory[getUUID(4,0)]>=1009 then
-        toCompress=math.floor((inventory[getUUID(4,0)]-1000)/9)
-        print("Compressing " .. 9*toCompress .. " cobblestone.")
-        ae.craft(getUUID(2506,0),toCompress)
-        jobsCount=jobsCount+toCompress
-    end
-    for i=0, 6 do
-        if inventory[getUUID(2506,i)]~=nil then 
-            toCompress=math.floor(inventory[getUUID(2506,i)]/9)
-            if toCompress>0 then
-                print("Compressing " .. 9*toCompress .." ".. (i+1) .."x compressed cobblestone.")
-                ae.craft(getUUID(2506,i+1),toCompress)
-                jobsCount=jobsCount+toCompress
+    if inventory~=nil then --Deal gracefuly with disconnects
+        jobsCount=0
+        if inventory[getUUID(4,0)]~=nil and inventory[getUUID(4,0)]>=1009 then
+            toCompress=math.floor((inventory[getUUID(4,0)]-1000)/9)
+            print("Compressing " .. 9*toCompress .. " cobblestone.")
+            ae.craft(getUUID(2506,0),toCompress)
+            jobsCount=jobsCount+toCompress
+        end
+        for i=0, 6 do
+            if inventory[getUUID(2506,i)]~=nil then 
+                toCompress=math.floor(inventory[getUUID(2506,i)]/9)
+                if toCompress>0 then
+                    print("Compressing " .. 9*toCompress .." ".. (i+1) .."x compressed cobblestone.")
+                    ae.craft(getUUID(2506,i+1),toCompress)
+                    jobsCount=jobsCount+toCompress
+                end
             end
         end
+        if jobsCount>0 then
+            cycle=cycle+1
+            print("Run "..cycle..": Executing ".. jobsCount .. " jobs.")
+        end
+        os.sleep(1+jobsCount/20)
+    else
+        os.sleep(1)
     end
-    if jobsCount>0 then
-        cycle=cycle+1
-        print("Run "..cycle..": Executing ".. jobsCount .. " jobs.")
-    end
-    os.sleep(1+jobsCount/20)
 end
 
