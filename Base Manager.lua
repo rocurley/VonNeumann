d64156dbd7f99fcdd61c71c2901a0c7b9db2e824
Roger Curley <rocurley@gmail.com>
2014-01-29 14:18:58 -0500

    Added software for base inventory management.

---
+++ Base Manager.lua
@@ -0,0 +1,26 @@
+ae=peripheral.wrap("bottom")
+inventory=ae.listItems()
+if inventory[getUUID(4,0)]>=1009 then
+    print("Compressing " .. 9*math.floor(inventory[getUUID(4,0)]-1000/9) " cobblestone.")
+    ae.craft(getUUID(2506,0),math.floor(inventory[getUUID(4,0)]-1000/9))
+end
+for i=0, 6 do
+    print("Compressing " .. 9*,math.floor(inventory[getUUID(2506,i)]/9) .." ".. (i+1) .."x compressed cobblestone.")
+    ae.craft(getUUID(2506,i+1),math.floor(inventory[getUUID(2506,i)]/9))
+end
+
+end
+function getID(uuid)
+  if uuid > 32768 then
+    id = uuid % bit.blshift(1,15) -- this line, originally done wrong by Nevercast, was corrected by JateZero
+    meta = bit.blogic_rshift(uuid-id,15) -- this line was corrected by JateZero
+  else
+    id = uuid
+    meta = 0
+  end
+  -- return id, meta
+end
+function getUUID(id, meta)
+   uuid = id + meta * 32768
+   return uuid
+ end
