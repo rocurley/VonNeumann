01ddbd762d7960a6ce5ca34a35f9d2acbb43e25a
Roger Curley <rocurley@gmail.com>
2014-01-29 14:24:16 -0500

    Putting functions at the top

--- Base Manager.lua
+++ Base Manager.lua
@@ -1,17 +1,3 @@
-ae=peripheral.wrap("bottom")
-inventory=ae.listItems()
-if inventory[getUUID(4,0)]>=1009 then
-    print("Compressing " .. 9*math.floor(inventory[getUUID(4,0)]-1000/9) " cobblestone.")
-    ae.craft(getUUID(2506,0),math.floor(inventory[getUUID(4,0)]-1000/9))
-end
-for i=0, 6 do
-    toCompress=math.floor(inventory[getUUID(2506,i)]/9)
-    if toCompress>0 then
-        print("Compressing " .. 9*toCompress .." ".. (i+1) .."x compressed cobblestone.")
-        ae.craft(getUUID(2506,i+1),toCompress)
-    end
-end
-
 function getID(uuid)
   if uuid > 32768 then
     id = uuid % bit.blshift(1,15) -- this line, originally done wrong by Nevercast, was corrected by JateZero
@@ -26,3 +12,18 @@ function getUUID(id, meta)
    uuid = id + meta * 32768
    return uuid
  end
+
+ae=peripheral.wrap("bottom")
+inventory=ae.listItems()
+if inventory[getUUID(4,0)]>=1009 then
+    print("Compressing " .. 9*math.floor(inventory[getUUID(4,0)]-1000/9) " cobblestone.")
+    ae.craft(getUUID(2506,0),math.floor(inventory[getUUID(4,0)]-1000/9))
+end
+for i=0, 6 do
+    toCompress=math.floor(inventory[getUUID(2506,i)]/9)
+    if toCompress>0 then
+        print("Compressing " .. 9*toCompress .." ".. (i+1) .."x compressed cobblestone.")
+        ae.craft(getUUID(2506,i+1),toCompress)
+    end
+end
+
