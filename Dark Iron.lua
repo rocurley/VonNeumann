bd43afa5d74d48c6f1604d0f70b9a3d1bd117196
Roger Curley <rocurley@gmail.com>
2014-01-30 01:26:53 -0500

    Dark iron automation

---
+++ Dark Iron.lua
@@ -0,0 +1,10 @@
+while true do
+    if turtle.getItemCount(1)>0 do
+        turtle.select(1)
+        turtle.place()
+        turtle.select(2)
+        os.sleep(.5)
+        turtle.dig()
+        turtle.dropDown()
+    end
+end
