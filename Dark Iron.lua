8fe5e0b59a4f32102d0648c03ed376754254d8fb
Roger Curley <rocurley@gmail.com>
2014-01-30 01:28:39 -0500

    .5->0.5

--- Dark Iron.lua
+++ Dark Iron.lua
@@ -3,7 +3,7 @@ while true do
         turtle.select(1)
         turtle.place()
         turtle.select(2)
-        os.sleep(.5)
+        os.sleep(0.5)
         turtle.dig()
         turtle.dropDown()
     end
